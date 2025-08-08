431dae7a37358b913ca28dcd0999966887829ac7
ArchchanaKrishnathas <archchana2504@gmail.com>
2025-08-08 13:40:36 +0530

    Add new views and update routes for pages
    Added new Blade views for home, about, contact, and gallery pages. Updated web routes to support these pages and made minor improvements.

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -5,3 +5,53 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+
+Route::get('/gallery', function () {
+    return view('pages.gallery');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+// Route::get('/about', function () {
+//     return view('pages.about');
+//    // return 'About page';
+// });
+
+
+// Route::view('/','welcome');
+
+// Route::view('/home','home');
+// Route::view('/gallery','layout.gallery');
+// Route::view('/contact','layout.contact');
+// Route::view('/about','layout.about');
+
+// Dynamic Routes
+
+Route::get('myPage/{name}',function($name){
+    return $name;
+});
+
+// This works dynamically 
+Route::get('pages/{page}',function($page){
+   return view('pages/' . $page);
+});
+
+// Route::get('pages/about/{name}',function($name){
+//     return view('pages/about',['my' => $name]);
+// });
+
+Route::get('pages/contact/{id}',function($id){
+    return view('pages/contact',['pnNumber' => $id]);
+});
+
+Route::get('pages/about/{name}/{color}',function($name,$color){
+    return view('pages/about',['my' => $name, 'bgColor' => $color]);
+});
